8f17ae9afd7229123229d9870f4755cabf70d17e
IS18game <61816406+IS18game@users.noreply.github.com>
2025-12-15 18:27:12 +0000

    Начало работы над 1 лабой

---
+++ Suslov/src/main/java/ru/rksp/Suslov/SuslovApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Suslov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuslovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuslovApplication.class, args);
+	}
+
+}
